02d04cb408cae8f21c318c9e27bf9e87e189045a
tanghongying <1483515764@qq.com>
2018-06-11 23:12:46 +0800

    修改

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,11 +21,11 @@ imageDatas = (function genImageURL(imageDatasArr) {
 class ImgFigure extends React.Component {
   render() {
     return (
-      <figure>
+      <figure className="img-figure">
           <img src={this.props.data.imageURL}
                alt={this.props.data.title} />
           <figcaption>
-              <h2>{this.props.data.title}</h2>
+              <h2 className="img-title">{this.props.data.title}</h2>
           </figcaption>
       </figure>
     )
